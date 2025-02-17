77ba91ca9834ff85b046d79a027152f243f91127
tbartsctr <tbartsctr@gmail.com>
2025-02-16 23:13:57 -0500

    BaseLayoutDesign

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="backgroundDiv">
+      <h1>Tucker's Weather Tracker</h1>
+    </div>
+  );
+}
+
+
+
+export default App
