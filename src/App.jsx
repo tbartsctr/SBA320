c18cc53c6d5824da5db48ac0e671906d64405ba0
tbartsctr <tbartsctr@gmail.com>
2025-02-17 00:12:24 -0500

    MapAdd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import { useState } from 'react'
+import { useEffect } from "react"
 import './App.css'
+import L from "leaflet";
+import 'leaflet/dist/leaflet.css'
 
 
 function App() {
-  return (
-    <div className="backgroundDiv">
-      <h1>Tucker's Weather Tracker</h1>
+  useEffect(() => {
+    const map = L.map('mapDiv').setView([39.12711000, -84.51439000], 4);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors',
+    }).addTo(map);
+
+    return() => {
+      map.remove();
+    };
+
+  }, []);
+
+  return(
+    <div>
+      <div id="mapDiv" style={{ height: '700px', width: "1000px"}}></div>
     </div>
   );
+  
 }
 
 
